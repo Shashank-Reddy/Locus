43ce4a0fb551705d00ba81afe1c330ea402e03e1
Shashank-Reddy <vemula.shashank@gmail.com>
2016-03-06 06:07:47 +0530

    Volley working, Markers Displayed

--- app/src/main/java/sara/locus/Drawer_Activity.java
+++ app/src/main/java/sara/locus/Drawer_Activity.java
@@ -25,29 +25,35 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.model.LatLng;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONStringer;
 
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
-import javax.xml.transform.ErrorListener;
 
+public class Drawer_Activity extends AppCompatActivity
+{
+    Map_Fragment mf=new Map_Fragment(); //create the fragment instance for the map fragment
 
-public class Drawer_Activity extends AppCompatActivity {
-    Map_Fragment mf = new Map_Fragment(); //create the fragment instance for the map fragment
-
-    List<LatLng> coords = new ArrayList<LatLng>(); //Used to store set of LatLngs received from server
-
-    Location mLocation = mf.mLocation;
-    double mLat = mLocation.getLatitude();
-    double mLng = mLocation.getLongitude();
+    List<LatLng> coords=new ArrayList<LatLng>(); //Used to store set of LatLngs received from server
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,8 +69,9 @@ public class Drawer_Activity extends AppCompatActivity {
             Uri photoURI = Uri.parse(sharedPreferences.getString("photoURI", null));
             ImageView userPhotoImageView = (ImageView) findViewById(R.id.userPhotoImageView);
             String url = photoURI.toString();
-            Picasso.with(getApplicationContext()).load(url).transform(new CircleTransform()).resize(120, 120).into(userPhotoImageView);
-        } catch (Exception e) {
+            Picasso.with(getApplicationContext()).load(url).transform(new CircleTransform()).resize(120,120).into(userPhotoImageView);
+        }
+        catch (Exception e) {
 
         }
 
@@ -75,8 +82,8 @@ public class Drawer_Activity extends AppCompatActivity {
         EmailTextView.setText(email);
 
         // Calling map_fragment from main(Drawer) activity
-        FragmentManager manager = getSupportFragmentManager();//create an instance of fragment manager
-        FragmentTransaction transaction = manager.beginTransaction();//create an instance of Fragment-transaction
+        FragmentManager manager=getSupportFragmentManager();//create an instance of fragment manager
+        FragmentTransaction transaction=manager.beginTransaction();//create an instance of Fragment-transaction
         transaction.add(R.id.map, mf, "Map_Fragment");
         transaction.commit();
         //
@@ -84,7 +91,8 @@ public class Drawer_Activity extends AppCompatActivity {
         String caller = getIntent().getStringExtra("caller");
         try {
             Class callerClass = Class.forName(caller);
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             e.getStackTrace();
         }
 
@@ -100,7 +108,7 @@ public class Drawer_Activity extends AppCompatActivity {
                         // continue with logout
                         SharedPreferences sharedPreferences = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
                         String name = sharedPreferences.getString("name", null);
-                        String byeText = "Goodbye, " + name;
+                        String byeText = "Goodbye, "+name;
                         Toast toast = Toast.makeText(getApplicationContext(),
                                 byeText,
                                 Toast.LENGTH_LONG);
@@ -110,7 +118,7 @@ public class Drawer_Activity extends AppCompatActivity {
                         editor.clear();
                         editor.apply();
 
-                        Intent i = new Intent(getApplicationContext(), Login_Activity.class);
+                        Intent i = new Intent(getApplicationContext(),Login_Activity.class);
                         startActivity(i);
                     }
                 })
@@ -130,80 +138,134 @@ public class Drawer_Activity extends AppCompatActivity {
         String caller = getIntent().getStringExtra("caller");
         try {
             Class callerClass = Class.forName(caller);
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             e.getStackTrace();
         }
         if (caller == "Login_Activity") {
 
-        } else {
+        }
+        else {
             moveTaskToBack(true);
         }
     }
 
-    public void sendToServer(View view) {
+    public void sendToServer(View view)
+    {
         EditText getDistance = (EditText) findViewById(R.id.eDistance); //eNTERED distance
-        String fDistance = getDistance.getText().toString();
+        String fDistance=getDistance.getText().toString();
 
 //        int fDistance=Integer.parseInt(getBudget.getText().toString());
 
 
         EditText getBudget = (EditText) findViewById(R.id.eBudget);
-        String fBudget = getBudget.getText().toString();
+        String fBudget=getBudget.getText().toString();
 
 //        int fBudget=Integer.parseInt(getBudget.getText().toString());
 
         EditText getHeadCount = (EditText) findViewById(R.id.eHeadCount);
-        String fHeadCount = getHeadCount.getText().toString();
+        String fHeadCount=getHeadCount.getText().toString();
 
 //        int fHeadCount=Integer.parseInt(getHeadCount.getText().toString());
+        Location mLocation=mf.mLocation;
 
+        double mLat=mLocation.getLatitude();
+        double mLng=mLocation.getLongitude();
 
-        LatLng mLatLng = new LatLng(mLat, mLng); //Eureka, it's working finally
+        LatLng mLatLng=new LatLng(mLat,mLng); //Eureka, it's working finally
 
 
         Toast toast = Toast.makeText(getApplicationContext(),
-                fDistance + "|" + fBudget + "|" + fHeadCount + "|" + mLatLng,
+                fDistance+"|"+fBudget+"|"+fHeadCount+"|"+mLatLng,
                 Toast.LENGTH_LONG);
         toast.setGravity(Gravity.BOTTOM, 0, 10);
         toast.show();
         //Control should go to the server from this point
         fromServer(fDistance, fBudget, fHeadCount, mLat, mLng); //Control should return from server to the Activity at this point
-        mf.displayMarkers();//This is used to display the markers based on LatLngs
+        geoCodeToServer(mLat,mLng); //API waale LAtLngs
+        mf.displayMarkers();//This is used to display the markers based on myLatLngs
 
 
     }
 
-    public void fromServer(String fDistance, String fBudget, String fHeadCount, double mLat, double mLng) {
+    public void fromServer(String fDistance, String fBudget, String fHeadCount, double mLat, double mLng)
+    {
 
-//        double Latitude[]={17.400384,17.400579}; //Get this from server
-//        double Longitude[]={78.489498,78.488653};//Get this from server (Test ke vaasthe KC,Agrawala)
-//
-//        //If we are using Json, we have to get set of LatLngs extracted from Json and push them into the List locally.
-//
-//        for (int i = 0 ; i < Latitude.length; i++)
-//                    {
-//                coords.add(new LatLng(Latitude[i], Longitude[i])); //adding items to LatLng List
-//                    }
+        double Latitude[]={17.400384,17.400579}; //Get this from server
+        double Longitude[]={78.489498,78.488653};//Get this from server (Test ke vaasthe KC,Agrawala)
 
-}
+        //If we are using Json, we have to get set of LatLngs extracted from Json and push them into the List locally.
 
-    public Request post(String Lat, String Lon,String url
-                        Response.Listener listener, Response.ErrorListener errorListener) {
-        Lat = new Double(mLat).toString();
-        Lon = new Double(mLng).toString();
-        url = "http://locus.arjun.ninja/api/geocode";
-        JSONObject params = new JSONObject();
-        params.put("lat", Lat);
-        params.put("lon", Lon);
-        Request req = new Request(
-                Method.POST,
-                url,
-                params.toString(),
-                listener,
-                errorListener
-        );
+        for (int i = 0 ; i < Latitude.length; i++)
+                    {
+                coords.add(new LatLng(Latitude[i], Longitude[i])); //adding items to LatLng List
+                    }
 
-        return req;
+    }
+
+    public void geoCodeToServer(final double mLat, final double mLng) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://locus.arjun.ninja/api/geocode";
+        final String sLat=Double.toString(mLat);
+        final String sLng=Double.toString(mLng);
+        final String Latitude[]=new String[100];
+        final String Longitude[]=new String[100];
+
+        StringRequest postRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                        try {
+                            JSONObject jsonRootObject = new JSONObject(response);
+                            //Get the instance of JSONArray that contains JSONObjects
+                            JSONArray jsonLatArray = jsonRootObject.getJSONArray("lats");
+                            JSONArray jsonLangArray=jsonRootObject.getJSONArray("lngs");
+
+                            double length=jsonLatArray.length();
+                            String len=Double.toString(length);
+                            Log.d("Length",len);
+
+                            //Iterate the jsonArray and print the info of JSONObjects
+                            for(int i=0; i < jsonLatArray.length(); i++)
+                            {
+//                                JSONObject jsonLatObject = jsonLatArray.getJSONObject(i); //something wrong here
+//                                Latitude[i]=jsonLatObject.toString();
+                                Latitude[i]=jsonLatArray.optString(i);
+                                double dLatitude=Double.parseDouble(Latitude[i]);
+//                                JSONObject jsonLngObject = jsonLangArray.getJSONObject(i);
+//                                Longitude[i]=jsonLngObject.toString();
+                                Longitude[i]=jsonLangArray.getString(i);
+                                double dLongitude=Double.parseDouble(Longitude[i]);
+                                coords.add(new LatLng(dLatitude, dLongitude));
+                            }
+
+                        }
+                        catch (JSONException e) {e.printStackTrace();}
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        Log.d("ERROR","error => "+error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("lat",sLat);
+                params.put("lng",sLng);
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
+
     }
 
 }
